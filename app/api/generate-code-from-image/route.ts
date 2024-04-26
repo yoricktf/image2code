2d245e466242758230c1caf81cb9af06a0a34e96
yoricktf <yorick.tenfeld@gmail.com>
2024-04-26 10:01:53 +0200

    removed the authentication requirement and started altering the prompt to make it viable tool for making HTML and CSS for email newsletters

--- app/api/generate-code-from-image/route.ts
+++ app/api/generate-code-from-image/route.ts
@@ -2,21 +2,21 @@ import OpenAI from 'openai'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 
 const SUPORTED_TECHNOLOGIES: any = {
-	Tailwind: "<script src='https://cdn.tailwindcss.com'></script>",
+  Tailwind: "<script src='https://cdn.tailwindcss.com'></script>",
 	Bootstrap:
-		"<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH' crossorigin='anonymous'>",
+  "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH' crossorigin='anonymous'>",
 	'Material Design':
-		"<link href='https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css' rel='stylesheet'> <script src='https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js'></script>",
+  "<link href='https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css' rel='stylesheet'> <script src='https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js'></script>",
 }
 
 const USER_PROMPT = 'Generate code for a web page that looks exactly like this'
 
 const SYSTEM_PROMPT = (technology: string) => `You are an expert ${technology} developer
-You take screenshots of a reference web page from the user, and then build single page apps 
+You take screenshots of a reference web page from the user, and then build single page apps
 using ${technology} components, HTML and JS.
 
 - Make sure the app looks exactly like the screenshot.
-- Pay close attention to background color, text color, font size, font family, 
+- Pay close attention to background color, text color, font size, font family,
 padding, margin, border, etc. Match the colors and sizes exactly.
 - Use the exact text from the screenshot.
 - Do not add comments in the code such as "<!-- Add other navigation links as needed -->" and "<!-- ... other news items ... -->" in place of writing the full code. WRITE THE FULL CODE.
@@ -27,7 +27,7 @@ padding, margin, border, etc. Match the colors and sizes exactly.
 - Take your time for the answer.
 
 In terms of libraries,
-${technology === 'CSS' ? '' : `- Use this script to include ${technology}: ${SUPORTED_TECHNOLOGIES[technology]}`} 
+${technology === 'CSS' ? '' : `- Use this script to include ${technology}: ${SUPORTED_TECHNOLOGIES[technology]}`}
 - You can use Google Fonts
 - Font Awesome for icons: <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"></link>
 
@@ -35,34 +35,36 @@ Return first the background hexadecimals, put a ||| separator, and then all the 
 Do not include markdown "\`\`\`" or "\`\`\`html" at the start or end.`
 
 const openai = new OpenAI({
-	apiKey: process.env.OPENAI_API_TOKEN,
+  apiKey: process.env.OPENAI_API_TOKEN,
 })
+
 
 export const runtime = 'edge'
 
 export async function POST(req: Request) {
-	const { url, img, technology } = await req.json()
+  console.log('================================================',process.env.OPENAI_API_TOKEN)
+  const { url, img, technology } = await req.json()
 	const technologySelected = technology ?? 'CSS'
 	const imageUrl = url ?? img
 
 	const response = await openai.chat.completions.create({
-		model: 'gpt-4-vision-preview',
+    model: 'gpt-4-vision-preview',
 		stream: true,
 		max_tokens: 4096,
 		messages: [
-			{
-				role: 'system',
+      {
+        role: 'system',
 				content: SYSTEM_PROMPT(technologySelected),
 			},
 			{
-				role: 'user',
+        role: 'user',
 				content: [
-					{
-						type: 'text',
+          {
+            type: 'text',
 						text: USER_PROMPT,
 					},
 					{
-						type: 'image_url',
+            type: 'image_url',
 						image_url: imageUrl,
 					},
 				],
--- app/page.tsx
+++ app/page.tsx
@@ -165,208 +165,206 @@ export default function Home() {
 	const [background, html = ''] = result.split('|||')
 	const isFirstTime = useFirstTimeCheck(Boolean(auth.currentUser))
 	return (
-		<ProtectedRoute>
-			<MotionPage initial="hide" animate="show" exit="hide" variants={pageTransitionAnim}>
-				<div className="grid grid-cols-[400px_1fr]">
-					<aside className="flex flex-col justify-between min-h-screen max-h-screen p-4 bg-gray-900 top-0 sticky">
-						<header className="text-center">
-							<h1 className="text-3xl font-semibold">Image 2 Code</h1>
-							<h2 className="text-md opacity-75 mt-2">Transform your images of the web to code in just seconds!</h2>
-							<div className="overflow-y-auto py-4 px-3 mt-4 bg-gray-50 rounded dark:bg-gray-800 h-auto">
-								<p className="mt-2 text-sm text-gray-600 dark:text-gray-400 text-justify ">
-									Welcome to our unique application, designed to take your web components images a step further by
-									transforming them into functional code. Powered by Chat GPT-4 and advanced image analysis
-									technologies, our platform interprets your images and converts them into HTML, JavaScript, and CSS,
-									Tailwind, Bootstrap or Mterial code structures, depending on your preferences.
-									<b> By default the application will use CSS as a reference.</b>
-								</p>
-								<h3 className="mt-4 text-md font-semibold text-gray-900 dark:text-white opacity-75">But remember!</h3>
-								<p className="mt-2 text-sm text-gray-600 dark:text-gray-400 text-justify">
-									If you dont pick any framework of your preference, by default the application is using just CSS.{' '}
-									<br /> We use https://placehold.co for components that uses images on it. So if you upload a wallpaper
-									or a draw, we can not give you some SVG of that... for now!
-								</p>
-							</div>
-						</header>
+		<MotionPage initial="hide" animate="show" exit="hide" variants={pageTransitionAnim}>
+			<div className="grid grid-cols-[400px_1fr]">
+				<aside className="flex flex-col justify-between min-h-screen max-h-screen p-4 bg-gray-900 top-0 sticky">
+					<header className="text-center">
+						<h1 className="text-3xl font-semibold">Image 2 Code</h1>
+						<h2 className="text-md opacity-75 mt-2">Transform your images of the web to code in just seconds!</h2>
+						<div className="overflow-y-auto py-4 px-3 mt-4 bg-gray-50 rounded dark:bg-gray-800 h-auto">
+							<p className="mt-2 text-sm text-gray-600 dark:text-gray-400 text-justify ">
+								Welcome to our unique application, designed to take your web components images a step further by
+								transforming them into functional code. Powered by Chat GPT-4 and advanced image analysis technologies,
+								our platform interprets your images and converts them into HTML, JavaScript, and CSS, Tailwind,
+								Bootstrap or Mterial code structures, depending on your preferences.
+								<b> By default the application will use CSS as a reference.</b>
+							</p>
+							<h3 className="mt-4 text-md font-semibold text-gray-900 dark:text-white opacity-75">But remember!</h3>
+							<p className="mt-2 text-sm text-gray-600 dark:text-gray-400 text-justify">
+								If you dont pick any framework of your preference, by default the application is using just CSS. <br />{' '}
+								We use https://placehold.co for components that uses images on it. So if you upload a wallpaper or a
+								draw, we can not give you some SVG of that... for now!
+							</p>
+						</div>
+					</header>
 
+					<section>
+						<h2 className="mb-4 text-xl font-semibold">Styles:</h2>
+
+						<div id="filters" className="flex flex-wrap items-center">
+							{Object.keys(ButtonColors).map((color, index) => (
+								<ToggleButton
+									key={index}
+									color={color as keyof typeof ButtonColors}
+									isActive={activeButton === color}
+									onClick={() => handleButtonClick(color, filterButtonsNames[index])}
+								>
+									{filterButtonsNames[index]}
+								</ToggleButton>
+							))}
+						</div>
+					</section>
+
+					<footer>
 						<section>
-							<h2 className="mb-4 text-xl font-semibold">Styles:</h2>
-
-							<div id="filters" className="flex flex-wrap items-center">
-								{Object.keys(ButtonColors).map((color, index) => (
-									<ToggleButton
-										key={index}
-										color={color as keyof typeof ButtonColors}
-										isActive={activeButton === color}
-										onClick={() => handleButtonClick(color, filterButtonsNames[index])}
-									>
-										{filterButtonsNames[index]}
-									</ToggleButton>
-								))}
+							<div id="signOut" className="flex flex-wrap items-center ">
+								<button
+									className="mr-2 mb-2 cursor-pointer rounded bg-red-600 px-2 py-1 text-white w-full text-center transition-all duration-100 --
+										hover:shadow-md border border-red-500 hover:bg-gradient-to-t hover:from-red-800 before:to-red-900
+										hover:-translate-y-[3px]"
+									type="button"
+									onClick={handleSignOut}
+								>
+									Sign Out
+								</button>
 							</div>
 						</section>
+						<span className="flex justify-center">
+							 Developed with ❤️ by <a href="https://github.com/jamoragar">Jamoragar</a>
+						</span>
+					</footer>
+				</aside>
 
-						<footer>
-							<section>
-								<div id="signOut" className="flex flex-wrap items-center ">
-									<button
-										className="mr-2 mb-2 cursor-pointer rounded bg-red-600 px-2 py-1 text-white w-full text-center transition-all duration-100 --
-											hover:shadow-md border border-red-500 hover:bg-gradient-to-t hover:from-red-800 before:to-red-900
-											hover:-translate-y-[3px]"
-										type="button"
-										onClick={handleSignOut}
+				<main className="bg-gray-950">
+					<section className="max-w-5xl w-full mx-auto p-10">
+						{step === STEPS.LOADING && (
+							<div className="flex justify-center items-center">
+								<div role="status">
+									<svg
+										aria-hidden="true"
+										className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+										viewBox="0 0 100 101"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
 									>
-										Sign Out
-									</button>
+										<path
+											d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+											fill="currentColor"
+										/>
+										<path
+											d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+											fill="currentFill"
+										/>
+									</svg>
+									<span className="sr-only">Loading...</span>
 								</div>
-							</section>
-							<span className="flex justify-center">
-								 Developed with ❤️ by <a href="https://github.com/jamoragar">Jamoragar</a>
-							</span>
-						</footer>
-					</aside>
+							</div>
+						)}
 
-					<main className="bg-gray-950">
-						<section className="max-w-5xl w-full mx-auto p-10">
-							{step === STEPS.LOADING && (
-								<div className="flex justify-center items-center">
-									<div role="status">
-										<svg
-											aria-hidden="true"
-											className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-											viewBox="0 0 100 101"
-											fill="none"
-											xmlns="http://www.w3.org/2000/svg"
-										>
-											<path
-												d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-												fill="currentColor"
-											/>
-											<path
-												d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-												fill="currentFill"
-											/>
-										</svg>
-										<span className="sr-only">Loading...</span>
-									</div>
+						{step === STEPS.INITIAL && (
+							<div className="flex flex-col gap-4">
+								<h2 className="mt-4 flex flex-col items-center text-2xl opacity-85">{activeButton && technology}</h2>
+
+								<div id="dragAndDrop">
+									<DragAndDrop transformImageToCode={transformImageToCode} />
 								</div>
-							)}
-
-							{step === STEPS.INITIAL && (
-								<div className="flex flex-col gap-4">
-									<h2 className="mt-4 flex flex-col items-center text-2xl opacity-85">{activeButton && technology}</h2>
-
-									<div id="dragAndDrop">
-										<DragAndDrop transformImageToCode={transformImageToCode} />
-									</div>
-									<div id="pasteUrl">
-										<Form transformUrlToCode={transformUrlToCode} />
-									</div>
+								<div id="pasteUrl">
+									<Form transformUrlToCode={transformUrlToCode} />
 								</div>
-							)}
+							</div>
+						)}
 
-							{step === STEPS.PREVIEW && (
-								<div className="rounded flex flex-col gap-4">
-									<div
-										className="w-full h-full border-4 rounded border-gray-700 aspect-video"
-										style={{ backgroundColor: `#${background ? background.trim() : 'fff'}` }}
-									>
-										<iframe srcDoc={html} className="w-full h-full" />
-									</div>
-									<pre className="pt-10">
-										<div className="w-full max-w-full max-h-full">
-											<div className="mb-2 flex justify-between items-center">
-												<p className="text-sm font-medium text-gray-900 dark:text-white">Your html code:</p>
+						{step === STEPS.PREVIEW && (
+							<div className="rounded flex flex-col gap-4">
+								<div
+									className="w-full h-full border-4 rounded border-gray-700 aspect-video"
+									style={{ backgroundColor: `#${background ? background.trim() : 'fff'}` }}
+								>
+									<iframe srcDoc={html} className="w-full h-full" />
+								</div>
+								<pre className="pt-10">
+									<div className="w-full max-w-full max-h-full">
+										<div className="mb-2 flex justify-between items-center">
+											<p className="text-sm font-medium text-gray-900 dark:text-white">Your html code:</p>
+										</div>
+										<div className="relative bg-gray-50 rounded-lg dark:bg-gray-700 p-4 h-80">
+											<div className="overflow-scroll max-h-full">
+												<pre>
+													<code id="code-block" className="text-sm text-gray-500 dark:text-gray-400 whitespace-pre">
+														{html}
+													</code>
+												</pre>
 											</div>
-											<div className="relative bg-gray-50 rounded-lg dark:bg-gray-700 p-4 h-80">
-												<div className="overflow-scroll max-h-full">
-													<pre>
-														<code id="code-block" className="text-sm text-gray-500 dark:text-gray-400 whitespace-pre">
-															{html}
-														</code>
-													</pre>
-												</div>
-												<div className="absolute top-2 end-2 bg-gray-50 dark:bg-gray-700">
-													<button
-														onClick={copyToClipboard}
-														className="text-gray-900 dark:text-gray-400 m-0.5 hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-600 dark:hover:bg-gray-700
-															rounded-lg py-2 px-2.5 inline-flex items-center justify-center bg-white border"
-													>
-														{copySuccess ? (
-															<span id="success-message" className="inline-flex items-center">
-																<svg
-																	className="w-3 h-3 text-blue-700 dark:text-blue-500 me-1.5"
-																	aria-hidden="true"
-																	xmlns="http://www.w3.org/2000/svg"
-																	fill="none"
-																	viewBox="0 0 16 12"
-																>
-																	<path
-																		stroke="currentColor"
-																		strokeLinecap="round"
-																		strokeLinejoin="round"
-																		strokeWidth="2"
-																		d="M1 5.917 5.724 10.5 15 1.5"
-																	/>
-																</svg>
-																<span className="text-xs font-semibold text-blue-700 dark:text-blue-500">Copied</span>
-															</span>
-														) : (
-															<span id="default-message" className="inline-flex items-center">
-																<svg
-																	className="w-3 h-3 me-1.5"
-																	aria-hidden="true"
-																	xmlns="http://www.w3.org/2000/svg"
-																	fill="currentColor"
-																	viewBox="0 0 18 20"
-																>
-																	<path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
-																</svg>
-																<span className="text-xs font-semibold">Copy code</span>
-															</span>
-														)}
-													</button>
-												</div>
+											<div className="absolute top-2 end-2 bg-gray-50 dark:bg-gray-700">
+												<button
+													onClick={copyToClipboard}
+													className="text-gray-900 dark:text-gray-400 m-0.5 hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-600 dark:hover:bg-gray-700
+														rounded-lg py-2 px-2.5 inline-flex items-center justify-center bg-white border"
+												>
+													{copySuccess ? (
+														<span id="success-message" className="inline-flex items-center">
+															<svg
+																className="w-3 h-3 text-blue-700 dark:text-blue-500 me-1.5"
+																aria-hidden="true"
+																xmlns="http://www.w3.org/2000/svg"
+																fill="none"
+																viewBox="0 0 16 12"
+															>
+																<path
+																	stroke="currentColor"
+																	strokeLinecap="round"
+																	strokeLinejoin="round"
+																	strokeWidth="2"
+																	d="M1 5.917 5.724 10.5 15 1.5"
+																/>
+															</svg>
+															<span className="text-xs font-semibold text-blue-700 dark:text-blue-500">Copied</span>
+														</span>
+													) : (
+														<span id="default-message" className="inline-flex items-center">
+															<svg
+																className="w-3 h-3 me-1.5"
+																aria-hidden="true"
+																xmlns="http://www.w3.org/2000/svg"
+																fill="currentColor"
+																viewBox="0 0 18 20"
+															>
+																<path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
+															</svg>
+															<span className="text-xs font-semibold">Copy code</span>
+														</span>
+													)}
+												</button>
 											</div>
 										</div>
-									</pre>
-								</div>
-							)}
-						</section>
-					</main>
-					{isFirstTime && auth && (
-						<ReactJoyride
-							steps={steps}
-							continuous
-							scrollToFirstStep
-							showProgress
-							showSkipButton
-							styles={{
-								options: {
-									// modal arrow and background color
-									arrowColor: '#eee',
-									backgroundColor: '#eee',
-									// page overlay color
-									overlayColor: 'rgba(0, 0, 0, 0.45)',
-									//button color
-									primaryColor: 'blue',
-									//text color
-									textColor: '#333',
+									</div>
+								</pre>
+							</div>
+						)}
+					</section>
+				</main>
+				{isFirstTime && auth && (
+					<ReactJoyride
+						steps={steps}
+						continuous
+						scrollToFirstStep
+						showProgress
+						showSkipButton
+						styles={{
+							options: {
+								// modal arrow and background color
+								arrowColor: '#eee',
+								backgroundColor: '#eee',
+								// page overlay color
+								overlayColor: 'rgba(0, 0, 0, 0.45)',
+								//button color
+								primaryColor: 'blue',
+								//text color
+								textColor: '#333',
 
-									//width of modal
-									width: 500,
-									//zindex of modal
-									zIndex: 1000,
-								},
-							}}
-						/>
-					)}
+								//width of modal
+								width: 500,
+								//zindex of modal
+								zIndex: 1000,
+							},
+						}}
+					/>
+				)}
 
-					<div id="reloadButton">
-						<ReloadButton />
-					</div>
+				<div id="reloadButton">
+					<ReloadButton />
 				</div>
-			</MotionPage>
-		</ProtectedRoute>
+			</div>
+		</MotionPage>
 	)
 }
